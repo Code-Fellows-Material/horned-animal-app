d3d0a126d1a4a2ae48f7ecbb3cbd891780709fec
Kellen-Linse <kblinse06@gmail.com>
2021-11-30 15:35:03 -0800

    add state and clickHandler for HornedBeast

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 
 class HornedBeast extends Component {
-  render() {
-    return (
-        <div>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.src} alt={'photo of ' + this.props.title} title={this.props.title + 'photo'}/>
-            <p>{this.props.description}</p>
-        </div>
-    )
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            likes: this.state.likes + 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.title}</h2>
+                <img onClick={this.handleClick} src={this.props.src} alt={'photo of ' + this.props.title} title={this.props.title + 'photo'}/>
+                <p> Likes: {this.state.likes}</p>
+                <p>{this.props.description}</p>
+            </div>
+        )
+    }
 }
 
 export default HornedBeast;
