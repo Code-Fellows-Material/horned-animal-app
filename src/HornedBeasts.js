c147b7cc6dace0708da524e44bfb5cfeb989ba0b
Kellen-Linse <kblinse06@gmail.com>
2021-12-02 23:25:18 -0800

    Hotfix 2.1 - Netlify

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 
 class HornedBeast extends Component {
